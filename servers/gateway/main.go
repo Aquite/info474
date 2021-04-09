be778193fabb517a9e153eda9baf6ec0647db4d1
Aquite <pavel.batalov@hotmail.com>
2021-04-08 20:03:11 -0700

    completed backend

--- servers/gateway/main.go
+++ servers/gateway/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	Handlers "assignments-Aquite/servers/gateway/handlers"
+	"log"
+	"net/http"
+	"os"
+)
+
 //main is the main entry point for the server
 func main() {
 	/* TODO: add code to do the following
@@ -13,5 +20,16 @@ func main() {
 	  the root handler. Use log.Fatal() to report any errors
 	  that occur when trying to start the web server.
 	*/
+
+	addr := os.Getenv("ADDR")
+	if len(addr) == 0 {
+		addr = ":80"
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/summary", Handlers.SummaryHandler)
+
+	log.Printf("server is listening at %s", addr)
+	log.Fatal(http.ListenAndServe(addr, mux))
 
 }
