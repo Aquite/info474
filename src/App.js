778d360e4ebed6ce5b9f3f1f3e59a042d2dbba8a
Aquite <pavel.batalov@hotmail.com>
2021-04-27 22:06:14 -0700

    fix browser router

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 import React from "react";
-import './App.css';
-import { Switch, Route, Redirect } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Header } from './Header.js';
-import Assignment2 from './assignment2/Assignment2.js';
-import Home from './Home.js';
-
+import "./App.css";
+import { Switch, Route, Redirect } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Header } from "./Header.js";
+import Assignment2 from "./assignment2/Assignment2.js";
+import Home from "./Home.js";
 
 const App = () => {
-
   return (
     <div className="App">
       <Header />
       <main>
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/assignment2" component={Assignment2} />
-          <Redirect to="/" />
+          <Route exact path="/info474" component={Home} />
+          <Route exact path="/info474/assignment2" component={Assignment2} />
+          <Redirect to="/info474" />
         </Switch>
       </main>
     </div>
   );
-}
-
+};
 
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import { LinkContainer } from 'react-router-bootstrap';
-
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { LinkContainer } from "react-router-bootstrap";
 
 export const Header = () => {
-
   return (
     <div>
-    <Navbar className="nav header" fixed="top" expand="lg" variant="dark">
-      <LinkContainer to="/"><Navbar.Brand>Pavel Batalov</Navbar.Brand></LinkContainer>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <LinkContainer to="/"><Nav.Link>Home</Nav.Link></LinkContainer>
-          <LinkContainer to="/assignment2"><Nav.Link>Assignment 2</Nav.Link></LinkContainer>
-        </Nav>
-      </Navbar.Collapse>
+      <Navbar className="nav header" fixed="top" expand="lg" variant="dark">
+        <LinkContainer to="/info474">
+          <Navbar.Brand>Pavel Batalov</Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <LinkContainer to="/info474">
+              <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/info474/assignment2">
+              <Nav.Link>Assignment 2</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </div>
   );
-}
+};
