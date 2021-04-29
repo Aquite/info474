f9aac7e02c6d512117e43b9f93f7d58fd38d3673
Aquite <pavel.batalov@hotmail.com>
2021-04-29 14:18:49 -0700

    store

--- servers/db/schema.sql
+++ servers/db/schema.sql
@@ -1,9 +1,9 @@
 create table if not exists users (
     ID int not null auto_increment primary key,
-    Email varchar(255) not null,
-    UserName varchar(255) not null,
+    Email varchar(255) not null UNIQUE,
+    UserName varchar(255) not null UNIQUE,
     PassHash varbinary(1024) not null,
     FirstName varchar(128) not null,
-    LastName varchar(64) not null,
+    LastName varchar(128) not null,
     PhotoURL varchar(128) not null
 );
