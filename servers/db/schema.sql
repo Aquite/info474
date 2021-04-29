857f2704b3ba1972e3dc9c8efcb6184217be7675
srinidhibalaraman <srinidhi.balaraman@gmail.com>
2021-04-29 12:56:28 -0700

    finised schema and customized docker DB image

--- servers/db/schema.sql
+++ servers/db/schema.sql
@@ -1,7 +1,7 @@
 create table if not exists users (
     ID int not null auto_increment primary key,
-    Email varchar(128) not null,
-    UserName varchar(64) not null,
+    Email varchar(255) not null,
+    UserName varchar(255) not null,
     PassHash varbinary(1024) not null,
     FirstName varchar(128) not null,
     LastName varchar(64) not null,
