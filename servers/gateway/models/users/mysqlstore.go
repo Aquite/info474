18803d5cf2e1f927e04100d86cb425774dcdf040
srinidhibalaraman <srinidhi.balaraman@gmail.com>
2021-04-29 18:26:18 -0700

    fixed mysqlstore code

--- servers/gateway/models/users/mysqlstore.go
+++ servers/gateway/models/users/mysqlstore.go
@@ -15,33 +15,6 @@ func NewSQLStore(DB *sql.DB) *SQLStore {
 }
 
 func GetByID(id int64) (*User, error) {
-
-}
-
-func createDB() {
-	dsn := fmt.Sprintf("root:%s@tcp(127.0.0.1:3306)/demo", os.Getenv("MYSQL_ROOT_PASSWORD"))
-
-	//create a database object, which manages a pool of
-	//network connections to the database server
-	db, err := sql.Open("mysql", dsn)
-	if err != nil {
-		fmt.Printf("error opening database: %v\n", err)
-		os.Exit(1)
-	}
-
-	//ensure that the database gets closed when we are done
-	defer db.Close()
-
-	//for now, just ping the server to ensure we have
-	//a live connection to it
-	if err := db.Ping(); err != nil {
-		fmt.Printf("error pinging database: %v\n", err)
-	} else {
-		fmt.Printf("successfully connected!\n")
-	}
-}
-
-func GetByID(id int64) (*User, error) {
 	q := "select * from users where id = ?"
 	rows, err := db.Query(q, id)
 	defer rows.Close()
