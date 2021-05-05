40d18aeebfa396221a8a91d4a5d37d29baee749e
Aquite <pavel.batalov@hotmail.com>
2021-05-05 15:43:36 -0700

    sessionstate struct

--- servers/gateway/handlers/sessionstate.go
+++ servers/gateway/handlers/sessionstate.go
@@ -1,5 +1,14 @@
 package handlers
 
+import (
+	"assignments-Aquite/servers/gateway/models/users"
+	"time"
+)
+
 //TODO: define a session state struct for this web server
 //see the assignment description for the fields you should include
 //remember that other packages can only see exported fields!
+type sessionState struct {
+	Time time.Time `json:"time"`
+	User users.User `json:"user"`
+}
