8478a436d64dc42d51487429a97b36bf4c0051dd
srinidhibalaraman <srinidhi.balaraman@gmail.com>
2021-05-08 18:04:29 -0700

    context.go

--- servers/gateway/handlers/context.go
+++ servers/gateway/handlers/context.go
@@ -1,8 +1,19 @@
 package handlers
 
+import (
+	"assignments-Aquite/servers/gateway/models/users"
+	"assignments-Aquite/servers/gateway/sessions"
+)
+
 //TODO: define a handler context struct that
 //will be a receiver on any of your HTTP
 //handler functions that need access to
 //globals, such as the key used for signing
 //and verifying SessionIDs, the session store
 //and the user store
+
+type HandlerContextStruct struct {
+	key       string
+	sessStore sessions.Store
+	userStore users.Store
+}
