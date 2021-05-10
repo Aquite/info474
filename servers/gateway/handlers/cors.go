eee8310a8f70a34a7dba1ac70a2a24ebb8627582
srinidhibalaraman <srinidhi.balaraman@gmail.com>
2021-05-10 01:09:40 -0700

    middleware

--- servers/gateway/handlers/cors.go
+++ servers/gateway/handlers/cors.go
@@ -1,5 +1,9 @@
 package handlers
 
+import (
+	"net/http"
+)
+
 /* TODO: implement a CORS middleware handler, as described
 in https://drstearns.github.io/tutorials/cors/ that responds
 with the following headers to all requests:
@@ -10,3 +14,21 @@ with the following headers to all requests:
   Access-Control-Expose-Headers: Authorization
   Access-Control-Max-Age: 600
 */
+
+type ResponseHeader struct {
+	handler      http.Handler
+	headerNames  []string
+	headerValues []string
+}
+
+func (l *ResponseHeader) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	for i := 0; i < len(headerNames); i++ {
+		w.Header().Add(rh.headerNames[i], rh.headerValue[i])
+	}
+
+	l.handler.ServeHTTP(w, r)
+}
+
+func NewResponseHeader(handlerToWrap http.Handler, headerName string, headerValue string) *ResponseHeader {
+	return &ResponseHeader{handlerToWrap, headerNames, headerValues}
+}
